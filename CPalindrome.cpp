6cff3472ffc85fe89addfc4cb2229194950115df
katarzynaadamczyk <kasia.cholost@gmail.com>
2020-10-26 22:42:08 +0100

    Changed main, added task if_one_letter_away_from_np, changed it and if_palidrome

--- CPalindrome.cpp
+++ CPalindrome.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include "CPalindrome.h"
 
 using namespace std;
+
+//**********************************************************************************************
 
 CPalindrome::CPalindrome()
 {
@@ -12,44 +15,112 @@ CPalindrome::~CPalindrome()
 {
 }
 
+
+//**********************************************************************************************
 //checks if given string is a palindrome
 bool CPalindrome::if_palindrome(const std::string &s)
 {
-    return false;
+    auto itb {s.begin()};
+    auto ite (s.end() - 1);
+    while (*itb == *ite && ite > itb)
+    {
+        ite--;
+        itb++;
+    }
+    if (ite > itb)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
+//**********************************************************************************************
 
+
+
+//**********************************************************************************************
 //returns minimum number of changes that are needed to change the given string into a palidrome (possible only inceasing or deacrising of letters)
 int CPalindrome::near_palindrome(const std::string &s)
 {
     return 0;
 }
-    
+//**********************************************************************************************
+
+
+//**********************************************************************************************
 //checks if there can be a removal of one letter to change the given string into a palidrome
-bool CPalindrome::create_palindrome(const std::string &s)
+bool CPalindrome::if_one_sign_away(const std::string &s)
 {
     return false;
 }
-    
+//**********************************************************************************************
+
+//**********************************************************************************************
+//checks if there can be a removal of one letter to change the given string into a near-palidrome
+bool CPalindrome::if_one_sign_away_from_np(const std::string &s)
+{
+    map<char,int> set;
+    for (auto it = s.begin(); it < s.end(); it++)
+    {
+        if (set.find(*it) == set.end())
+        {
+            set[*it] = 1;
+        }
+        else
+        {
+            set.find(*it)->second++;
+        }
+    }
+    int odd_count {};
+    for (auto it = set.begin(); it != set.end(); it++)
+    {
+        if (it->second % 2 != 0)
+        {
+            odd_count++;
+        }
+    }
+    if ((s.size() % 2 == 0 && odd_count < 3) || (s.size() % 2 == 1 && odd_count < 2))
+    {
+        return true;
+    }
+    return false;
+}
+//**********************************************************************************************
+
+
+//**********************************************************************************************
 //rearranges letters in given string to make a palindrome, if it is not possible return "None"
 string CPalindrome::find_palindrome(std::string &s)
 {
     return "";
 }
-    
+//**********************************************************************************************
+
+
+//**********************************************************************************************
 //finds the longest palidrome in given string, if there is none return "None"
 string CPalindrome::longest_palindrome(std::string &s)
 {
     return "";
 }
-    
+//**********************************************************************************************
+
+
+//**********************************************************************************************
 //returns the smallest alphabetically string that is a near palindrome, doing as few operations as possible (possible is only changing of letter into a different one)
 string CPalindrome::even_palindrome(std::string &s)
 {
     return "";
 }
+//**********************************************************************************************
 
+
+//**********************************************************************************************
 //checks if given integer is a palindrome
 bool CPalindrome::int_is_palindrome(int &i)
 {
     return false;
 }
+//**********************************************************************************************
--- main.cpp
+++ main.cpp
@@ -6,6 +6,31 @@ using namespace std;
 
 int main(void)
 {
-    
+    cout << "Welcome to Palindromic Quests program" << endl;
+    string s;
+    while (s != "end")
+    {
+        cout << "Please enter the string that will be checked if it is a palindrome, if you write \"end\" the program will end." << endl;
+        cout << "Your input: ";
+        getline(cin, s);
+        if (CPalindrome::if_palindrome(s))
+        {
+            cout << "Given string is a palindrome" << endl;
+        }
+        else
+        {
+            cout << "Given string is not a palindrome" << endl;
+        }
+        if (CPalindrome::if_one_sign_away_from_np(s))
+        {
+            cout << "In given string one letter can be removed to create a near-palindrome" << endl;
+        }
+        else
+        {
+            cout << "Given string cannot be changed to a near-palidrome by removing one letter" << endl;
+        }
+        
+        cout << endl;
+    }
     return 0;
 }
